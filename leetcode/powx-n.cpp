5267eae9c1774908ab88f504dd8061baaa98c84c
甘春雨 <keyboard3@icloud.com>
2021-06-08 09:12:44 +0800

    重新思考powx-n的规律实现c++

---
+++ leetcode/powx-n.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    double myPow(double x, long long n)
+    {
+
+        return n > 0 ? pow(x, n) : 1 / pow(x, -n);
+    }
+    double pow(double x, long long n)
+    {
+        double y = x, ans = 1;
+        while (n > 0)
+        {
+            if (n % 2 == 1)
+                ans *= y; //核心多余 1->2->4 = 2*4*16 = 2^7 = 128
+            n = n / 2;
+            y *= y;
+        }
+        return ans;
+    }
+};
+
+//  7->3->1->0
+//  1->1*[2]->(2*2)*[2]->(([2]*[2])*[2])*(([2]*[2])*[2])*[2]
+//  查看规律发现奇数的多余计算是反向规律的
+//  7->3->1->0
+//  1->2->4 = 2*4*16 = 2^7 = 128
--- leetcode/powx-n.js
+++ leetcode/powx-n.js
@@ -17,10 +17,18 @@ function powRecursion(x, n) {
 function powWhile(x, n) {
   let y = x, ans = 1;
   while (n > 0) {
-    if (n % 2 == 1) ans *= y;//最后*y，起始奇数y
-    y = y * y;
+    if (n % 2 == 1) ans *= y;//核心 1->2->4 = 2*4*16 = 2^7 = 128
     n = parseInt(n / 2);
+    y *= y;
   }
   return ans;
 }
-console.log(myPow(2, 10));
+console.log(myPow(2, 6));
+
+/**
+* 7->3->1->0
+* 1->1*[2]->(2*2)*[2]->(([2]*[2])*[2])*(([2]*[2])*[2])*[2]
+* 查看规律发现奇数的多余计算是反向规律的
+* 7->3->1->0
+* 1->2->4 = 2*4*16 = 2^7 = 128
+*/
