179048294b35a4fa75df20e2b607125bbf441770
甘春雨 <keyboard3@icloud.com>
2021-06-13 15:33:23 +0800

    正则匹配提供基于动态规划枚举出所有状态的实现

--- leetcode/regular-expression-matching.cpp
+++ leetcode/regular-expression-matching.cpp
@@ -1,10 +1,42 @@
 #include "string"
+#include "vector"
 using namespace std;
 
 class Solution
 {
 public:
   bool isMatch(string s, string p)
+  {
+    vector<vector<bool>> state(p.length() + 1, vector<bool>(s.length() + 1));
+    state[0][0] = true;
+    auto matchs = [&](int pi, int si)
+    {
+      if (si == 0) //为了特殊处理边界.* c*等
+        return false;
+      return p[pi - 1] == '.' || p[pi - 1] == s[si - 1];
+    };
+    for (int pi = 1; pi <= p.length(); pi++)
+    {
+      if (p[pi - 1] == '*')
+      {
+        state[pi] = state[pi - 1];
+        continue;
+      }
+      for (int si = 0; si <= s.length(); si++) //为了特殊处理边界.* c*等
+      {
+        if (pi < p.length() && p[pi] == '*')
+        {
+          state[pi][si] = state[pi - 1][si];
+          if (matchs(pi, si))
+            state[pi][si] = state[pi][si] || state[pi][si - 1];
+        }
+        else if (matchs(pi, si))
+          state[pi][si] = state[pi - 1][si - 1];
+      }
+    }
+    return state[p.length()][s.length()];
+  }
+  bool isMatch2(string s, string p)
   {
     return dfs(s, 0, p, 0);
   }
--- leetcode/regular-expression-matching.js
+++ leetcode/regular-expression-matching.js
@@ -1,16 +1,41 @@
-/**
- * @param {string} s
- * @param {string} p
- * @return {boolean}
- */
+//采用枚举出所有状态，从目标结果倒推出组合路径
 var isMatch = function (s, p) {
-  return dfs(0, 0, "");
-  function dfs(si, pi, tab) {
-    // console.log(tab + "dfs:", si, pi, s.slice(0, si + 1), p.slice(0, pi + 1))
-    if (si >= s.length && pi >= p.length) {
-      // console.log("找到");
-      return true;
+  let state = [[]];
+
+  state[0][0] = true;
+  //枚举出所有路径i,j的匹配情况，第i个匹配可以根据前面的匹配状态，计算当前自己的能够匹配的情况
+  for (let pi = 1; pi <= p.length; pi++) {
+    state[pi] = [];
+    if (p[pi - 1] == "*") {
+      state[pi] = state[pi - 1];
+      continue;
     }
+    for (let si = 0; si <= s.length; si++) {
+      state[pi][si] = false;
+      if (p[pi] == "*") {
+        state[pi][si] = state[pi - 1][si];
+        if (matchChr(pi, si)) {//会尝试匹配个空
+          state[pi][si] |= state[pi][si - 1];
+        }
+      } else {
+        if (matchChr(pi, si)) {
+          state[pi][si] = state[pi - 1][si - 1];
+        }
+      }
+    }
+  }
+  return !!state[p.length][s.length];
+  function matchChr(pi, si) {
+    if (si == 0) return false;
+    if (p[pi - 1] == "." || p[pi - 1] == s[si - 1]) return true;
+    return false;
+  }
+}
+//采用递归枚举组合出目标结果
+var isMatch2 = function (s, p) {
+  return dfs(0, 0, "");
+  function dfs(si, pi) {
+    if (si >= s.length && pi >= p.length) return true;
 
     let matchChr = p[pi];
     let range = [1, 1];
@@ -21,30 +46,29 @@ var isMatch = function (s, p) {
 
     let matchNum = 0;
     do {
-      // console.log(tab + "while", s[si], matchChr)
-      if (matchNum == 0 && range[0] == 0 && dfs(si, pi, tab + "\t")) return true;
+      if (matchNum == 0 && range[0] == 0 && dfs(si, pi)) return true;
 
       if (!s[si]) return false;
       if (matchChr == "." || matchChr == s[si]) {
         matchNum++;
-        if (dfs(++si, pi, tab + "\t")) return true;
+        if (dfs(++si, pi)) return true;
       } else break;
     } while (matchNum < range[1]);
-    // console.log(tab + "失败");
     return false;
   }
 };
 
 let array = [
-  [true, "a", "ab*"],//[a]a a
-  [false, "aa", "a"],//[a]a a
+  [true, "a", "a"],//[a]a a
+  [true, "aac", "a*c"],//[a]a a
   [true, "ab", ".*"], //[ab] .*
   [true, "aab", "c*a*b"],//[aa][b]
   [true, "aaa", "a*a"],//a[a]a
   [false, "mississippi", "mis*is*p*."],
   [false, "ippi", "p*."],
   [false, "ab", "a*"],
-  [false, "a", ".*..a*"]
+  [false, "a", ".*..a*"],
+  [true, "aasdfasdfasdfasdfas", "aasdf.*asdf.*asdf.*asdf.*s"],
 ]
 for (let [result, ...params] of array) {
   let mr = isMatch(...params);
