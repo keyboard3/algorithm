9cc9467e152fa9d48cd81f692a0f13adb0b586eb
甘春雨 <keyboard3@icloud.com>
2021-06-13 21:53:17 +0800

    优化盛最多水的容器

--- leetcode/container-with-most-water.cpp
+++ leetcode/container-with-most-water.cpp
@@ -1,29 +1,46 @@
+#include "vector"
+#include "cmath"
+using namespace std;
+
 class Solution
 {
 public:
+  //优化方案，设定找到两端短边和长边，计算完面积之后，再以原来的长边作为短边继续向内找它的长边。
   int maxArea(vector<int> &height)
+  {
+    int minI = 0, maxI = height.size() - 1;
+    int mArea = 0, direction = -1;
+    int temp;
+    auto swapI = [&]()
+    {
+      temp = minI;
+      minI = maxI, maxI = temp;
+      direction = -direction;
+    };
+    if (height[minI] > height[maxI])
+      swapI();
+    while (minI != maxI)
+    {
+      mArea = max(mArea, min(height[minI], height[maxI]) * abs(maxI - minI));
+      if (height[maxI] >= height[minI])
+        swapI();
+      maxI += direction;
+    }
+    return mArea;
+  }
+  //不断枚举左边的为短边，从右边依次找到长边。在找长边的过程中同时可以间接计算面积更新。缺点是每次左边枚举值，右边就得从头匹配
+  int maxArea2(vector<int> &height)
   {
     int mArea = 0;
     for (int i = 0; i < height.size(); i++)
       for (int j = height.size() - 1; j > i; j--)
       {
         if (height[j] < height[i])
+        {
+          mArea = max(height[j] * (j - i), mArea);
           continue;
-
-        int area = height[i] * (j - i);
-        if (area > mArea)
-          mArea = area;
-        break;
-      }
-    for (int i = height.size() - 1; i >= 0; i--)
-      for (int j = 0; j < i; j++)
-      {
-        if (height[j] < height[i])
-          continue;
-
-        int area = height[i] * (i - j);
-        if (area > mArea)
-          mArea = area;
+        }
+        mArea = max(height[i] * (j - i), mArea);
         break;
       }
     return mArea;
--- leetcode/container-with-most-water.js
+++ leetcode/container-with-most-water.js
@@ -1,24 +1,35 @@
-/**
- * @param {number[]} height
- * @return {number}
- */
+
+
+//优化方案，设定找到两端短边和长边，计算完面积之后，再以原来的长边作为短边继续向内找它的长边。
 var maxArea = function (height) {
+  let mArea = 0;
+  let minI = 0, maxI = height.length - 1;
+  let direction = -1;
+
+  if (height[minI] > height[maxI]) {
+    [maxI, minI] = [minI, maxI];
+    direction = -direction;
+  }
+  while (minI != maxI) {
+    mArea = Math.max(mArea, Math.min(height[minI], height[maxI]) * Math.abs(maxI - minI));
+    if (height[maxI] >= height[minI]) {
+      direction = -direction;
+      [maxI, minI] = [minI, maxI];
+    }
+    maxI += direction;
+  }
+  return mArea;
+};
+//不断枚举左边的为短边，从右边依次找到长边。在找长边的过程中同时可以间接计算面积更新。缺点是每次左边枚举值，右边就得从头匹配
+var maxArea2 = function (height) {
   let mArea = 0;
   for (let i = 0; i < height.length; i++) {
     for (let j = height.length - 1; j > i; j--) {
-      if (height[j] < height[i]) continue;
-
-      let area = height[i] * (j - i);
-      if (area > mArea) mArea = area;
-      break;
-    }
-  }
-  for (let i = height.length - 1; i >= 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (height[j] < height[i]) continue;
-
-      let area = height[i] * (i - j);
-      if (area > mArea) mArea = area;
+      if (height[j] < height[i]) {
+        mArea = Math.max(height[j] * (j - i), mArea);
+        continue;
+      }
+      mArea = Math.max(height[i] * (j - i), mArea);
       break;
     }
   }
