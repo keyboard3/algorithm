fb5df31788e8d2cd288117f95c2b3ac5fa9a08b6
甘春雨 <keyboard3@icloud.com>
2021-06-14 20:59:31 +0800

    优化算法逻辑

--- leetcode/3sum.cpp
+++ leetcode/3sum.cpp
@@ -6,14 +6,65 @@ using namespace std;
 class Solution
 {
 public:
+  //通过去重之后，负数区枚举两个在正数区找第3个。正数区枚举两个，找负数区第3个。这种自然的处理就不会导致重复加入结果
   vector<vector<int>> threeSum(vector<int> &nums)
   {
     vector<vector<int>> result;
-    if(nums.size()<3) return result;
-    
+    sort(nums.begin(), nums.end());
+    map<int, int> showNumMap;
+    vector<int> distNums;
+    for (int i = 0; i < nums.size(); i++)
+    {
+      int times = 0;
+      if (showNumMap.find(nums[i]) != showNumMap.end())
+        times = showNumMap.at(nums[i]);
+      showNumMap.erase(nums[i]);
+      showNumMap.emplace(nums[i], times + 1);
+      if (times == 0)
+        distNums.push_back(nums[i]);
+    }
+    auto append = [&](int &firstVal, int &secondVal)
+    {
+      int threeVal = -firstVal - secondVal;
+      if (showNumMap.find(threeVal) == showNumMap.end())
+        return;
+      result.push_back(vector({firstVal, secondVal, threeVal}));
+    };
+    auto appendSame = [&](int &firstVal)
+    {
+      int times = showNumMap.at(firstVal);
+      if (times <= 1 || (firstVal == 0 && times < 3))
+        return;
+      append(firstVal, firstVal);
+    };
+    //正数区枚举
+    int first = 0;
+    for (; first < distNums.size() && distNums[first] < 0; first++)
+    {
+      appendSame(distNums[first]);
+      for (int second = first + 1; second < distNums.size() && distNums[second] < 0; second++)
+        append(distNums[first], distNums[second]);
+    }
+
+    //负数区枚举
+    for (; first < distNums.size(); first++)
+    {
+      appendSame(distNums[first]);
+      for (int second = first + 1; second < distNums.size(); second++)
+        append(distNums[first], distNums[second]);
+    }
+    return result;
+  };
+  //一个数通过Map映射查找，另外两个数通过枚举组合。并特殊处理高重复的元素情况
+  vector<vector<int>> threeSum2(vector<int> &nums)
+  {
+    vector<vector<int>> result;
+    if (nums.size() < 3)
+      return result;
+
     sort(nums.begin(), nums.end());
     vector<int> newNums;
-    
+
     int prev = INT_MAX, sameNum = 0;
     for (int i = 0; i < nums.size(); i++)
     {
@@ -22,7 +73,9 @@ public:
         sameNum++;
         if (sameNum > 3)
           continue;
-      } else sameNum = 0;
+      }
+      else
+        sameNum = 0;
       prev = nums[i];
       newNums.push_back(nums[i]);
     }
@@ -34,27 +87,29 @@ public:
         array = numMap.at(newNums[i]);
       array.push_back(i);
       numMap.erase(newNums[i]);
-      numMap.emplace(newNums[i],array);
+      numMap.emplace(newNums[i], array);
     }
 
     map<string, bool> showedMap;
-    for (int i = 0; i < newNums.size()-2; i++)
-      for (int j = i + 1; j < newNums.size()-1; j++)
+    for (int i = 0; i < newNums.size() - 2; i++)
+      for (int j = i + 1; j < newNums.size() - 1; j++)
       {
         vector<int> third;
         if (numMap.find(-newNums[i] - newNums[j]) != numMap.end())
           third = numMap.at(-newNums[i] - newNums[j]);
-        if(third.size()==0) continue;
+        if (third.size() == 0)
+          continue;
 
         vector ele({newNums[i],
                     newNums[j],
                     -newNums[i] - newNums[j]});
         sort(ele.begin(), ele.end());
         string key = to_string(ele[0]) + "," + to_string(ele[1]) + "," + to_string(ele[2]);
-        if (showedMap.find(key) != showedMap.end()) {
-            continue;
+        if (showedMap.find(key) != showedMap.end())
+        {
+          continue;
         }
-         
+
         for (int t = 0; t < third.size(); t++)
         {
           if (third[t] <= j)
@@ -65,5 +120,5 @@ public:
         }
       }
     return result;
-  }
+  };
 };
--- leetcode/3sum.js
+++ leetcode/3sum.js
@@ -1,11 +1,45 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
+
+//通过去重之后，负数区枚举两个在正数区找第3个。正数区枚举两个，找负数区第3个。这种自然的处理就不会导致重复加入结果
 var threeSum = function (nums) {
   let result = [];
+  nums.sort((a, b) => a - b);
+  const showNumMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let times = showNumMap.get(nums[i]) || 0;
+    showNumMap.set(nums[i], times + 1);
+  }
+  const distNums = [...showNumMap.keys()]
+  let first = 0;
+  //枚举负数区
+  for (first = 0; distNums[first] < 0; first++) {
+    appendSame(distNums[first]);
+    for (let second = first + 1; distNums[second] < 0; second++)
+      append(distNums[first], distNums[second]);
+  }
+  //枚举正数区
+  for (; first < distNums.length; first++) {
+    appendSame(distNums[first]);
+    for (let second = first + 1; second < distNums.length; second++)
+      append(distNums[first], distNums[second]);
+  }
+  return result;
+  function appendSame(firstVal) {
+    let times = showNumMap.get(firstVal);
+    if (times <= 1 || (firstVal == 0 && times < 3)) return;
+    append(firstVal, firstVal);
+  }
+  function append(firstVal, secondVal) {
+    let threeVal = -firstVal - secondVal;
+    if (!showNumMap.get(threeVal)) return;
+    result.push([firstVal, secondVal, threeVal]);
+  }
+}
+
+//一个数通过Map映射查找，另外两个数通过枚举组合。并特殊处理高重复的元素情况
+var threeSum2 = function (nums) {
+  let result = [];
   const map = new Map();
-  const arrayMap = new Map();
+  const inArrayMap = new Map();
   nums.sort((a, b) => a - b);
   const newNums = [];
   let prev, sameNum = 0;
@@ -27,10 +61,10 @@ var threeSum = function (nums) {
       let third = map.get(-newNums[i] - newNums[j]);
       if (!third?.length) continue;
       let ele = [newNums[i], newNums[j], -newNums[i] - newNums[j]].sort((a, b) => a - b);
-      if (arrayMap.get(ele.join(""))) continue;
+      if (inArrayMap.get(ele.join(""))) continue;
       for (let three of third) {
         if (three <= j) continue;
-        arrayMap.set(ele.join(""), true);
+        inArrayMap.set(ele.join(""), true);
         result.push(ele);
         break;
       }
