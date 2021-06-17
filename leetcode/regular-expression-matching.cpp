c5e969813e7890a3ad91f01135e431c3db7c8a9d
甘春雨 <keyboard3@icloud.com>
2021-06-17 21:20:40 +0800

    尝试优化c++的dp空间 正则匹配

--- leetcode/regular-expression-matching.cpp
+++ leetcode/regular-expression-matching.cpp
@@ -5,9 +5,85 @@ using namespace std;
 class Solution
 {
 public:
+  ///dp的反向递归写法优化（实际空间不减反增）
   bool isMatch(string s, string p)
   {
-    vector<vector<bool>> state(p.length() + 1, vector<bool>(s.length() + 1));
+    reverse(p.begin(), p.end());
+    bool *state = negativeDfs(s, p);
+    return state[s.length()];
+  }
+  bool *negativeDfs(string &s, string p)
+  {
+    if (p.empty())
+    {
+      bool state[s.length() + 1];
+      memset(&state, 0, (s.length() + 1) * sizeof(bool));
+      state[0] = true;
+      return state;
+    }
+    bool state[s.length() + 1];
+    memset(&state, 0, (s.length() + 1) * sizeof(bool));
+    int len = 1;
+    char mChr = p[0];
+    if (p[0] == '*')
+      len = 2, mChr = p[1];
+    bool *preState = negativeDfs(s, p.substr(len));
+    for (int i = 0; i <= s.length(); i++)
+    {
+      if (len > 1)
+      {
+        state[i] = preState[i];
+        if (matchChr(s, mChr, i))
+          state[i] |= state[i - 1];
+      }
+      else if (matchChr(s, mChr, i))
+        state[i] = preState[i - 1];
+    }
+    return state;
+  }
+  //dp的正向递归写法优化（实际空间不减反增）
+  bool isMatch4(string s, string p)
+  {
+    bool preState[s.length() + 1];
+    memset(&preState, 0, (s.length() + 1) * sizeof(bool));
+    preState[0] = true;
+    return positiveDfs(s, p, preState);
+  }
+  bool positiveDfs(string &s, string p, bool prevState[])
+  {
+    if (p.empty())
+      return prevState[s.length()];
+    int len = 1;
+    if (p.length() > 1 && p[1] == '*')
+      len = 2;
+    bool state[s.length() + 1];
+    memset(&state, 0, (s.length() + 1) * sizeof(bool));
+    for (int i = 0; i <= s.length(); i++)
+    {
+      if (len == 2)
+      {
+        state[i] = prevState[i];
+        if (matchChr(s, p[0], i))
+          state[i] |= state[i - 1];
+      }
+      else if (matchChr(s, p[0], i))
+        state[i] = prevState[i - 1];
+    }
+    return positiveDfs(s, p.substr(len), state);
+  }
+  bool matchChr(string &s, char chr, int index)
+  {
+    if (index == 0)
+      return false;
+    if (chr == '.' || chr == s[index - 1])
+      return true;
+    return false;
+  }
+  //dp枚举出前一个匹配串对应所有字符的匹配状态
+  bool isMatch3(string s, string p)
+  {
+    bool state[p.length() + 1][s.length() + 1];
+    memset(&state, 0, (p.length() + 1) * (s.length() + 1) * sizeof(bool));
     state[0][0] = true;
     auto matchs = [&](int pi, int si)
     {
@@ -19,23 +95,25 @@ public:
     {
       if (p[pi - 1] == '*')
       {
-        state[pi] = state[pi - 1];
+        copy(state[pi - 1], state[pi - 1] + s.length() + 1, state[pi]);
         continue;
       }
+      int prevIndex = pi - 1;
       for (int si = 0; si <= s.length(); si++) //为了特殊处理边界.* c*等
       {
         if (pi < p.length() && p[pi] == '*')
         {
-          state[pi][si] = state[pi - 1][si];
+          state[pi][si] = state[prevIndex][si];
           if (matchs(pi, si))
             state[pi][si] = state[pi][si] || state[pi][si - 1];
         }
         else if (matchs(pi, si))
-          state[pi][si] = state[pi - 1][si - 1];
+          state[pi][si] = state[prevIndex][si - 1];
       }
     }
     return state[p.length()][s.length()];
   }
+  //回溯枚举目标匹配
   bool isMatch2(string s, string p)
   {
     return dfs(s, 0, p, 0);
